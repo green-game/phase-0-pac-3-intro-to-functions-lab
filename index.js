983cc61a95c672366164e3ecd88b492bef5a40b5
Allan Monzon <greengamer.am@gmail.com>
2022-10-26 16:24:29 -0400

    commit tree 058b4470e7c62537c1f66caa2d7acd99713fd387

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string){
+   return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logshout(string){
+    console.log(shout);
+}
+function logWhisper(string){
+    console.log(whisper);
+}
+function sayHiToHeadphonedRoommate(string){
+    return string.toLowerCase();
+}
